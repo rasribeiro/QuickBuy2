6d5d18e5b40a184b13d812df73f089c7a6faf302
Ricardo Ribeiro <ralexandre_rn@hotmail.com>
2020-03-31 23:40:33 -0300

    Criacao de entidades

---
+++ QuickBuy.Dominio/ObjetoDeValor/FormaPagamento.cs
@@ -0,0 +1,27 @@
+﻿namespace QuickBuy.Dominio.ObjetoDeValor
+{
+    public class FormaPagamento
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+        public bool EhBoleto
+        {
+            get { return Id == (Int)Enumerados.TipoFormaPagamentoEnum.Boleto; }
+        }
+
+        public bool EhCartaoCredito
+        {
+            get { return Id == (Int)Enumerados.TipoFormaPagamentoEnum.CartaoCredito; }
+        }
+        public bool EhDeposito
+        {
+            get { return Id == (Int)Enumerados.TipoFormaPagamentoEnum.Deposito; }
+        }
+        public bool NaoFoiDefinido
+        {
+            get { return Id == (Int)Enumerados.TipoFormaPagamentoEnum.NaoDefinico; }
+        }
+    }
+}
